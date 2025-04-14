ee0150b0b64fd14fc923ec925f3f76db53d906a6
Leecifer <128757198+Leescifer@users.noreply.github.com>
2025-04-14 09:10:34 +0800

    Delete server directory

--- server/config/env.js
+++
@@ -1,7 +0,0 @@
-import { config } from 'dotenv';
-
-// Ensure a default NODE_ENV is used if it's not set
-const envFile = `.env.${process.env.NODE_ENV || 'development'}.local`;
-config({ path: envFile });
-
-export const { PORT, NODE_ENV } = process.env;
--- server/server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import { PORT } from './config/env.js'
-
-const app = express();
-
-app.listen(PORT, () => {
-    console.log(`Casptone API is running on http://:${PORT}`)
-})
