2f18d6f8848e4d1e8e421e6508db33020f8b8e27
Leecifer <128757198+Leescifer@users.noreply.github.com>
2025-04-16 19:17:39 +0800

    Updates for the ui shadcn and devdependencies

--- client/src/pages/LandingPage.jsx/sections/hero.jsx
+++ client/src/pages/LandingPage.jsx/sections/hero.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Button } from '@/components/ui/button.jsx'
 
 const Hero = () => {
   return (
     <section id='hero' className=''>
       
+      <main className=''>
+        <Button>gelli</Button>
+
+
+      </main>
+
 
     </section>
   )
